a5c80b96456455a75a765e24072be427a447aa81
Ast1o465 <dimam4659@gmail.com>
2026-01-24 16:39:06 +0200

    created a function for connecting a client to a server

--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -6,9 +6,36 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    m_socket = new QTcpSocket(this);
+
+    ui->Le_ip->setPlaceholderText("127.0.0.1");
+    ui->Le_port->setPlaceholderText("8888");
+    ui->L_status->setStyleSheet("color: red");
+
+    connect(ui->Btn_connect, &QPushButton::clicked, this, &MainWindow::connectToServer);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::connectToServer()
+{
+    if (!m_socket) {
+        m_socket = new QTcpSocket(this);
+    }
+
+    QString ip = ui->Le_ip->text();
+    int port = ui->Le_port->text().toInt();
+
+    m_socket->connectToHost(ip, port);
+    if (m_socket->waitForConnected()) {
+        ui->L_status->setStyleSheet("color: green");
+        ui->L_status->setText("Connected to server.");
+    } else {
+        ui->L_status->setStyleSheet("color: red");
+        ui->L_status->setText("Failed to connect to server.");
+    }
 }
--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +20,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void connectToServer();
+
 private:
     Ui::MainWindow *ui;
+    QTcpSocket *m_socket;
 };
 #endif // MAINWINDOW_H
