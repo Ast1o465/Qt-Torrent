865ce6c39a0340c9a8769c4dd7aa398f24ec8a51
Ast1o465 <dimam4659@gmail.com>
2026-01-24 16:58:52 +0200

    made it so that the disconnect button works and so that it is visible in the interface that you have disconnected from the server

--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -9,11 +9,13 @@ MainWindow::MainWindow(QWidget *parent)
 
     m_socket = new QTcpSocket(this);
 
+    ui->Btn_disconnect->setEnabled(false);
     ui->Le_ip->setPlaceholderText("127.0.0.1");
     ui->Le_port->setPlaceholderText("8888");
     ui->L_status->setStyleSheet("color: red");
 
     connect(ui->Btn_connect, &QPushButton::clicked, this, &MainWindow::connectToServer);
+    connect(ui->Btn_disconnect, &QPushButton::clicked, this, &MainWindow::disconnectFromServer);
 }
 
 MainWindow::~MainWindow()
@@ -23,19 +25,28 @@ MainWindow::~MainWindow()
 
 void MainWindow::connectToServer()
 {
-    if (!m_socket) {
-        m_socket = new QTcpSocket(this);
-    }
-
     QString ip = ui->Le_ip->text();
     int port = ui->Le_port->text().toInt();
 
     m_socket->connectToHost(ip, port);
     if (m_socket->waitForConnected()) {
+        ui->Btn_connect->setEnabled(false);
+        ui->Btn_disconnect->setEnabled(true);
+
         ui->L_status->setStyleSheet("color: green");
         ui->L_status->setText("Connected to server.");
     } else {
         ui->L_status->setStyleSheet("color: red");
         ui->L_status->setText("Failed to connect to server.");
     }
+}
+
+void MainWindow::disconnectFromServer()
+{
+    ui->L_status->setText("You are disconnected");
+    ui->L_status->setStyleSheet("color: orange");
+
+    ui->Btn_connect->setEnabled(true);
+    ui->Btn_disconnect->setEnabled(false);
+    m_socket->disconnectFromHost();
 }
--- Client/mainwindow.h
+++ Client/mainwindow.h
@@ -22,6 +22,7 @@ public:
 
 private slots:
     void connectToServer();
+    void disconnectFromServer();
 
 private:
     Ui::MainWindow *ui;
