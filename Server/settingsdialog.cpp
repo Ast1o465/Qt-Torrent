b402d7f7ee97bd2e2ba553efdd62aaf630ec53d7
Ast1o465 <dimam4659@gmail.com>
2026-01-28 18:37:07 +0200

    finished developing the functionality of the new UI for changing server settings

--- Server/settingsdialog.cpp
+++ Server/settingsdialog.cpp
@@ -14,8 +14,10 @@ settingsdialog::settingsdialog(QWidget *parent)
     ui->Sc_Le_port->setText(settings.value("Network/Port", "8888").toString());
     ui->SC_Le_dir->setText(settings.value("Storage/Path", "./server_files").toString());
 
-    // connect(ui->Btn_cancel, &QPushButton::clicked, this, &settingsdialog::saveAndClose);
-    // connect(ui->Tb_dir, &QToolButton::clicked, this, &settingsdialog::selectFolder);
+    connect(ui->Btn_save, &QPushButton::clicked, this, &settingsdialog::saveAndClose);
+    connect(ui->Btn_save, &QPushButton::clicked, this, &settingsdialog::reject);
+    connect(ui->Tb_dir, &QToolButton::clicked, this, &settingsdialog::selectFolder);
+
 }
 
 settingsdialog::~settingsdialog()
@@ -25,10 +27,27 @@ settingsdialog::~settingsdialog()
 
 void settingsdialog::saveAndClose()
 {
+    QString configPath = QCoreApplication::applicationDirPath() + "/config.ini";
+    QSettings settings(configPath, QSettings::IniFormat);
 
+    settings.beginGroup("Network");
+    settings.setValue("Address", ui->Sc_Le_IP->text());
+    settings.setValue("Port", ui->Sc_Le_port->text());
+    settings.endGroup();
+
+    settings.beginGroup("Storage");
+    settings.setValue("Path", ui->SC_Le_dir->text());
+    settings.endGroup();
+
+    settings.sync();
+
+    accept();
 }
 
 void settingsdialog::selectFolder()
 {
-
+    QString dir = QFileDialog::getExistingDirectory(this, "Select Folder", ui->SC_Le_dir->text());
+    if (!dir.isEmpty()) {
+        ui->SC_Le_dir->setText(dir);
+    }
 }
